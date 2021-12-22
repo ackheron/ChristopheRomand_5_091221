883dbbe97660cc46eff90c7e4455cd3b7c1bc930
Christophe Romand <ackheron@tuta.io>
2021-12-22 14:45:33 +0100

    URLSearchParams, insertion produit page product et sélection couleurs

--- js/script.js
+++ js/script.js
@@ -4,31 +4,6 @@ const kanapApi = "http://localhost:3000/api/products";
 
 // Requête HTTP de type GET vers l'api
 
-// fetch(kanapApi)
-//   .then((response) => response.json())
-
-//   .then((products) => {
-//     products.forEach((data) => {
-//       console.log(data);
-//       document.getElementById(
-//         "items"
-//       ).innerHTML += `<a href="./product.html?id=${data._id}">
-//              <article>
-//                 <img
-//                     src="${data.imageUrl}"
-//                     alt="${data.altTxt}"/>
-//                 <h3 class="productName"> ${data.name}</h3>
-//                 <p class="productDescription"> ${data.description}</p>
-//             </article>
-//         </a>`;
-//     });
-//   })
-//   .catch((error) =>
-//     console.log(
-//       "Il y a eu un problème avec l'opération fetch: " + error.message
-//    )
-//   );
-
 fetch(kanapApi)
   .then((response) => {
     return response.json();
@@ -53,8 +28,5 @@ fetch(kanapApi)
   })
 
   .catch((err) => {
-    console.log("test d'erreur");
-    document.getElementById(
-      "items"
-    ).innerHTML += `<h3>Erreur de chargment</h3>`;
+    alert(error);
   });
