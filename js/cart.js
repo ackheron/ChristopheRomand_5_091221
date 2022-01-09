09bb196f1a4afd955aa00e8b943140f05e69be8d
Ackheron <ackheron@etik.com>
2022-01-09 17:22:41 +0100

    MOD bouton retour et messages erreurs formulaire

--- js/cart.js
+++ js/cart.js
@@ -78,6 +78,8 @@ function injectSommeQuant() {
   //Injection de la somme totale dans le DOM
   document.querySelector("#totalPrice").textContent = sommeTotale;
 
+  localStorage.setItem("sommeTotale", JSON.stringify(sommeTotale));
+
   // Appel de la fonction addQuantFunction qui nous retourne la variable quant
   let quantTotale = addQuantFunction();
 
@@ -179,9 +181,14 @@ btnCommander.addEventListener("click", (event) => {
     let inputFirstName = document.querySelector("#firstName");
     if (regExPrenomNomVille(prenom)) {
       inputFirstName.style.backgroundColor = "green";
+
+      document.querySelector("#firstNameErrorMsg").textContent = "";
       return true;
     } else {
       inputFirstName.style.backgroundColor = "#FF6F61";
+
+      document.querySelector("#firstNameErrorMsg").textContent =
+        "Champ Prénom de formulaire invalide, ex: Paul";
       return false;
     }
   }
@@ -190,9 +197,14 @@ btnCommander.addEventListener("click", (event) => {
     let inputLastName = document.querySelector("#lastName");
     if (regExPrenomNomVille(nom)) {
       inputLastName.style.backgroundColor = "green";
+
+      document.querySelector("#lastNameErrorMsg").textContent = "";
       return true;
     } else {
       inputLastName.style.backgroundColor = "#FF6F61";
+
+      document.querySelector("#lastNameErrorMsg").textContent =
+        "Champ Nom de formulaire invalide, ex: Durand";
       return false;
     }
   }
@@ -202,9 +214,14 @@ btnCommander.addEventListener("click", (event) => {
     let inputAddress = document.querySelector("#address");
     if (regExAdresse(adresse)) {
       inputAddress.style.backgroundColor = "green";
+
+      document.querySelector("#addressErrorMsg").textContent = "";
       return true;
     } else {
       inputAddress.style.backgroundColor = "#FF6F61";
+
+      document.querySelector("#addressErrorMsg").textContent =
+        "Champ Adresse de formulaire invalide, ex: 50 rue de la paix";
       return false;
     }
   }
@@ -213,9 +230,14 @@ btnCommander.addEventListener("click", (event) => {
     let inputCity = document.querySelector("#city");
     if (regExPrenomNomVille(ville)) {
       inputCity.style.backgroundColor = "green";
+
+      document.querySelector("#cityErrorMsg").textContent = "";
       return true;
     } else {
       inputCity.style.backgroundColor = "#FF6F61";
+
+      document.querySelector("#cityErrorMsg").textContent =
+        "Champ Ville de formulaire invalide, ex: Paris";
       return false;
     }
   }
@@ -224,9 +246,14 @@ btnCommander.addEventListener("click", (event) => {
     let inputMail = document.querySelector("#email");
     if (regExEmail(courriel)) {
       inputMail.style.backgroundColor = "green";
+
+      document.querySelector("#emailErrorMsg").textContent = "";
       return true;
     } else {
       inputMail.style.backgroundColor = "#FF6F61";
+
+      document.querySelector("#emailErrorMsg").textContent =
+        "Champ Email de formulaire invalide, ex: example@contact.fr";
       return false;
     }
   }
@@ -242,6 +269,8 @@ btnCommander.addEventListener("click", (event) => {
     // Enregistrer le formulaire dans le local storage
     localStorage.setItem("contact", JSON.stringify(contact));
 
+    document.querySelector("#order").value =
+      "Articles et formulaire valide\n Passer commande !";
     sendToServer();
   } else {
     console.log("Veuillez bien remplir le formulaire");
@@ -277,9 +306,12 @@ btnCommander.addEventListener("click", (event) => {
 let dataFormulaire = JSON.parse(localStorage.getItem("contact"));
 
 console.log(dataFormulaire);
-
-document.querySelector("#firstName").value = dataFormulaire.firstName;
-document.querySelector("#lastName").value = dataFormulaire.lastName;
-document.querySelector("#address").value = dataFormulaire.address;
-document.querySelector("#city").value = dataFormulaire.city;
-document.querySelector("#email").value = dataFormulaire.email;
+if (dataFormulaire) {
+  document.querySelector("#firstName").value = dataFormulaire.firstName;
+  document.querySelector("#lastName").value = dataFormulaire.lastName;
+  document.querySelector("#address").value = dataFormulaire.address;
+  document.querySelector("#city").value = dataFormulaire.city;
+  document.querySelector("#email").value = dataFormulaire.email;
+} else {
+  console.log("Le formulaire est vide");
+}
